87552cd0c80d6b77cbee94abefeff167d04e6eb4
JimNuttUp <jim@upcyclepost.com>
2015-10-28 16:58:44 -0700

    Moved data to separate js file

--- app.js
+++ app.js
@@ -23,40 +23,4 @@
 		};
 	});
 	
-	var gems = [ 
-	{
-		name: 'Dodecahedron',
-		price: 2,
-		description: 'This is the Dodecahedron',
-		canPurchase: true,
-		soldOut:true,
-		review:'this is a cool gem',
-		images: [
-			{
-				full: 'img/dodecahedron-01-full.jpg',
-				thumb: 'img/dodecahedron-01-thumb.png'
-			}
-		],
-		reviews: [
-			{
-				stars:5,
-				body:"I love this product!",
-				author: "joe@thomas.com"
-			},
-			{
-				stars:1,
-				body:"this product sucks",
-				author: "jane@thomas.com"
-			}
-		],
-	},
-	{
-		name: 'Pentagonal Gem',
-		price: 5.95,
-		description: 'This is the Pentagonal Gem',
-		canPurchase: false,
-		soldOut:true,
-	}
-
-	];
 })();
